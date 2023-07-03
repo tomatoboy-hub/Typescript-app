5cc30ce743a65c3527efe0d85c1eb50dd4d636e4
Hayata Shimizu <hayata@TakayanoMacBook-Air.local>
2023-07-03 23:23:40 +0900

    random-cat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,50 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import {GetServerSideProps,NextPage} from "next";
+import {useEffect, useState} from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+type Props = {
+    initialImageUrl: string;
+}
+type Image = {
+    url: string;
+};
 
-export default IndexPage
+const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
+    const [imageUrl,setImageUrl] = useState(initialImageUrl);
+    const [loading,setLoading] = useState(false);
+    // useEffect(() =>{
+    //     fetchImage().then((newImage) =>{
+    //         setImageUrl(newImage.url);
+    //         setLoading(false);
+    //     });
+    // },[]);
+    const handleClick = async() => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url);
+        setLoading(false);
+    }
+    return (
+    <div className = {styles.page}>
+        <button onClick = {handleClick} className={styles.button}>他のニャンコも見る</button>
+        <div className = {styles.frame}>{loading || <img src={imageUrl}/>}</div>
+    </div>
+)};
+export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async() => {
+    const image = await fetchImage();
+    return{
+        props:{
+            initialImageUrl: image.url,
+        },
+    };
+};
+
+const fetchImage = async():Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+}
+
